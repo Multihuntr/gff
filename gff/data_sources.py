Now uses the implementaion of utae from coastalFlood so we have access to lead time. Updated two utae to use lead time. Fixed data loading for ERA5 and ERA5L indices off and scaling not applied correctly. Tiny update to ERA5 downloader script: now checks for file existing before asking google about it.

gff/data_sources.py
@@ -15,6 +15,7 @@ import zipfile
 import asf_search as asf
 
 import geopandas
+import numpy as np
 import pandas
 import rasterio
 import shapely
@@ -283,13 +284,13 @@ def load_dfo(path: Path, for_s1: bool = False):
 def _era5_band_index(p: Path):
     band_index = {}
     with rasterio.open(p) as tif:
-        for i, name in tif.descriptions:
+        for i, name in enumerate(tif.descriptions):
             parts = name.split("-")
-            day = "-".join(parts[:2])
+            day = "-".join(parts[:3])
             k = parts[-1]
             if day not in band_index:
                 band_index[day] = {}
-            band_index[day][k] = i
+            band_index[day][k] = i + 1
     return band_index
 
 
@@ -304,16 +305,16 @@ def load_era5(
 ):
     if keys is None:
         if era5_land:
-            keys = constants.ERA5L_BAND
+            keys = constants.ERA5L_BANDS
         else:
             keys = constants.ERA5_BANDS
     results = []
     current = start
     while current <= end:
         if era5_land:
-            fname = f"era5-land-{current.year}-{current.month}.tif"
+            fname = f"era5-land-{current.year}-{current.month:02d}.tif"
         else:
-            fname = f"era5-{current.year}-{current.month}.tif"
+            fname = f"era5-{current.year}-{current.month:02d}.tif"
         band_index = _era5_band_index(folder / fname)
         day_str = current.strftime("%Y-%m-%d")
         band_idxs = [band_index[day_str][k] for k in keys]
@@ -327,7 +328,10 @@ def load_era5(
                 out_shape=(res, res),
                 resampling=rasterio.enums.Resampling.bilinear,
             )
-            data = data * tif.scales[:, None, None] + tif.offsets[:, None, None]
+            band_idxs0 = np.array(band_idxs) - 1  # Make them 0-indexed
+            scales = np.array(tif.scales)[band_idxs0, None, None]
+            offsets = np.array(tif.offsets)[band_idxs0, None, None]
+            data = data * scales + offsets
         results.append(data)
         current += datetime.timedelta(days=1)
 
@@ -335,11 +339,13 @@ def load_era5(
 
 
 def load_pregenerated_raster(
-    fpath: Path, geom: shapely.Geometry, res: int, keys: list[str] = None
+    fpath: Path, geom: shapely.Geometry, res: int, keys: list[str] | list[int] = None
 ):
     with rasterio.open(fpath) as tif:
         if keys is None:
-            band_idxs = list(range(len(tif.descriptions)))
+            band_idxs = list(range(1, len(tif.descriptions) + 1))
+        elif isinstance(keys[0], int):
+            band_idxs = keys
         else:
             band_idxs = [tif.descriptions.index(k) + 1 for k in keys]
         window = util.shapely_bounds_to_rasterio_window(geom.bounds, tif.transform, align=False)

gff/dataloaders.py
@@ -54,19 +54,24 @@ class DebugFloodForecastDataset(torch.utils.data.Dataset):
         if "s1" in C["data_sources"]:
             self.s1 = np.random.randn(self.n_examples, 2, 224, 224)
             self.s1 = self.s1.astype(np.float32)
+            self.s1_lead_days = np.random.randint(0, 60, (self.n_examples,))
+            self.s1_lead_days = self.s1_lead_days.astype(np.int64)
 
     def __len__(self):
         return self.n_examples
 
     def __getitem__(self, idx):
         raster_data = {k: getattr(self, k)[idx] for k in self.data_sources}
-        return {
+        example = {
             "floodmap": self.floodmaps[idx],
             "continent": 1,
             "geom": self.bogus_geom,
             "context_geom": self.bogus_geom,
             **raster_data,
         }
+        if "s1" in example:
+            example["s1_lead_days"] = self.s1_lead_days[idx]
+        return example
 
 
 class FloodForecastDataset(torch.utils.data.Dataset):
@@ -84,34 +89,36 @@ class FloodForecastDataset(torch.utils.data.Dataset):
         self.floodmap_path = self.folder / "floodmaps" / self.C["floodmap"]
         self.is_train = is_train
         self.split = split
-        self.incl_list = self.read_partition()
+        self.meta_fnames = self.read_partition()
         self.metas = self.load_tile_metas()
         self.tiles = self.load_tile_geoms(self.metas)
 
     def read_partition(self):
         # Split index defines the partition index to use as test.
         test_partition_fname = f"floodmap_partition_{self.split}.txt"
-        partition_paths = list((self.folder / "partitions").glob("floodmap_partition_?.txt"))
-        self.floodmap_fnames = []
+        partition_paths = list((self.folder / "partitions/").glob("floodmap_partition_?.txt"))
+        meta_fnames = []
         for fpath in partition_paths:
             partition_is_train = fpath.name != test_partition_fname
             if self.is_train == partition_is_train:
                 fnames = pandas.read_csv(fpath, header=None)[0].values.tolist()
-                self.floodmap_fnames.extend(fnames)
+                meta_fnames.extend(fnames)
+        return meta_fnames
 
     def mk_context_geom(self, geom: shapely.Geometry):
         w = h = gff.constants.CONTEXT_DEGREES
-        lon, lat = geom.centroid
+        lon, lat = shapely.get_coordinates(geom.centroid)[0]
         lonlo, latlo = lon - w / 2, lat + h / 2
         lonhi, lathi = lon + w / 2, lat - h / 2
         return shapely.box(lonlo, latlo, lonhi, lathi)
 
     def load_tile_metas(self):
         metas = []
-        for meta_path in self.floodmap_fnames:
-            with open(meta_path) as f:
+        for meta_fname in self.meta_fnames:
+            with open(self.floodmap_path / meta_fname) as f:
                 meta = json.load(f)
             metas.append(meta)
+        return metas
 
     def load_tile_geoms(self, metas: list[dict]):
         tiles = []
@@ -129,43 +136,52 @@ class FloodForecastDataset(torch.utils.data.Dataset):
             crs_parallel = [visit_tiles.crs] * len(geoms)
             pairs = list(zip(meta_parallel, crs_parallel, geoms, context_geoms))
             tiles.extend(pairs)
+        return tiles
 
     def get_s1_path(self, meta):
-        if self.s1_exported:
+        if self.C["use_exported_s1"]:
             s1_folder = "s1-export"
         else:
             s1_folder = "s1"
 
         date_str = datetime.datetime.fromisoformat(meta["pre1_date"]).strftime("%Y-%m-%d")
         if meta["type"] == "kurosiwo":
-            s1_fname = f"{meta['info']['actiid']}-{meta['info']['aoiid']}-{date_str}.tif"
+            fname = f"{meta['info']['actiid']}-{meta['info']['aoiid']}-{date_str}.tif"
+            s1_fname = "kurosiwo" / fname
         elif meta["type"] == "generated":
             s1_fname = f"{meta['FLOOD']}-{meta['HYBAS_ID']}-{date_str}.tif"
 
         return self.folder / s1_folder / s1_fname
+
+    def get_s1_lead_days(self, meta):
+        pre1_date = datetime.datetime.fromisoformat(meta["pre1_date"])
+        post_date = datetime.datetime.fromisoformat(meta["post_date"])
+        return (post_date - pre1_date).days
 
     def __len__(self):
         return len(self.tiles)
 
     def __getitem__(self, idx):
         meta, crs, geom, context_geom = self.tiles[idx]
-        floodmap_path = self.floodmap_path / meta["floomaps"]
+        floodmap_path = self.floodmap_path / meta["floodmap"]
         s1_path = self.get_s1_path(meta)
         hybas_key = "HYBAS_ID" if "HYBAS_ID" in meta else "HYBAS_ID_4"
         continent = int(str(meta[hybas_key])[0])
         result = {
-            "floodmap": gff.util.get_tile(floodmap_path, geom),
+            "floodmap": gff.util.get_tile(floodmap_path, geom.bounds),
             "continent": continent,
             "geom": geom,
             "context_geom": context_geom,
+            "s1_lead_days": self.get_s1_lead_days(meta),
         }
 
         # Add in various data sources
         # Weather data
-        last_name = gff.constants.KUROSIWO_S1_NAMES[-2]
-        next_name = gff.constants.KUROSIWO_S1_NAMES[-1]
-        weather_start = datetime.datetime.fromisoformat(meta[f"{last_name}_date"])
-        weather_end = datetime.datetime.fromisoformat(meta[f"{next_name}_date"])
+        # last_name = gff.constants.KUROSIWO_S1_NAMES[-2]
+        future_name = gff.constants.KUROSIWO_S1_NAMES[-1]
+        # weather_start = datetime.datetime.fromisoformat(meta[f"{last_name}_date"])
+        weather_end = datetime.datetime.fromisoformat(meta[f"{future_name}_date"])
+        weather_start = weather_end - datetime.timedelta(days=20)
         if "era5_land" in self.C["data_sources"]:
             result["era5_land"] = gff.data_sources.load_era5(
                 self.ext_folders["era5_land"],
@@ -175,6 +191,7 @@ class FloodForecastDataset(torch.utils.data.Dataset):
                 weather_end,
                 era5_land=True,
             )
+            result["era5_land"] = np.array(result["era5_land"], dtype=np.float32)
         if "era5" in self.C["data_sources"]:
             result["era5"] = gff.data_sources.load_era5(
                 self.ext_folders["era5"],
@@ -184,13 +201,18 @@ class FloodForecastDataset(torch.utils.data.Dataset):
                 weather_end,
                 era5_land=False,
             )
+            result["era5"] = np.array(result["era5"], dtype=np.float32)
 
         # (Relatively) static soil attributes
         if "hydroatlas_basin" in self.C["data_sources"]:
             fpath = self.folder / gff.constants.HYDROATLAS_RASTER_FNAME
             result["hydroatlas_basin"] = gff.data_sources.load_pregenerated_raster(
-                fpath, context_geom, gff.constants.CONTEXT_RESOLUTION, keys=self.hydroatlas_keys
+                fpath,
+                context_geom,
+                gff.constants.CONTEXT_RESOLUTION,
+                keys=self.C["hydroatlas_keys"],
             )
+            result["hydroatlas_basin"] = np.array(result["hydroatlas_basin"], dtype=np.float32)
 
         # DEM at coarse and fine scales
         if "dem_context" in self.C["data_sources"]:
@@ -198,15 +220,17 @@ class FloodForecastDataset(torch.utils.data.Dataset):
             result["dem_context"] = gff.data_sources.load_pregenerated_raster(
                 fpath, context_geom, gff.constants.CONTEXT_RESOLUTION
             )
+            result["dem_context"] = np.array(result["dem_context"], dtype=np.float32)
         if "dem_local" in self.C["data_sources"]:
             dem = gff.data_sources.get_dem(geom, crs, self.folder)
             result["dem_local"] = gff.util.resample_xr(
-                dem, geom, gff.constants.CONTEXT_RESOLUTION, method="linear"
-            )
+                dem, geom.bounds, (gff.constants.LOCAL_RESOLUTION,) * 2, method="linear"
+            ).band_data.values
+            result["dem_local"] = np.array(result["dem_local"], dtype=np.float32)
 
         # Sentinel 1 (assumed to be a proxy for soil moisture)
         if "s1" in self.C["data_sources"]:
-            result["s1"] = gff.util.get_tile(s1_path, geom)
+            result["s1"] = gff.util.get_tile(s1_path, geom.bounds)
 
         return result
 
@@ -219,7 +243,7 @@ def custom_collate_fn(original_batch, as_list=["continent", "geom", "context_geo
             if key in as_list:
                 result[key].append(value)
             else:
-                result[key].append(torch.tensor(value))
+                result[key].append(torch.tensor(np.array(value)))
     for key in keys:
         if key not in as_list:
             result[key] = torch.stack(result[key])
@@ -227,15 +251,17 @@ def custom_collate_fn(original_batch, as_list=["continent", "geom", "context_geo
 
 
 def create(C, generator):
+    data_folder = Path(C["data_folder"]).expanduser()
     if C["dataset"] == "debug_dataset":
-        train_ds = DebugFloodForecastDataset(C["data_folder"], C)
-        test_ds = DebugFloodForecastDataset(C["data_folder"], C)
+        train_ds = DebugFloodForecastDataset(data_folder, C)
+        test_ds = DebugFloodForecastDataset(data_folder, C)
     elif C["dataset"] == "forecast_dataset":
+        ext_data_folders = {k: Path(p).expanduser() for k, p in C["ext_data_folders"].items()}
         train_ds = FloodForecastDataset(
-            C["data_folder"], C["ext_folders"], C, is_train=True, split=C["test_partition"]
+            data_folder, ext_data_folders, C, is_train=True, split=C["split"]
         )
         test_ds = FloodForecastDataset(
-            C["data_folder"], C["ext_folders"], C, is_train=False, split=C["test_partition"]
+            data_folder, ext_data_folders, C, is_train=False, split=C["split"]
         )
     else:
         raise NotImplementedError(f"Dataset {C['dataset']} not supported")

gff/models/two_utae.py
@@ -5,47 +5,144 @@ from . import utae
 
 
 class TwoUTAE(nn.Module):
-    def __init__(self, n_hydroatlas, hydroatlas_dim, n_context, n_predict=3):
+    def __init__(
+        self,
+        n_weather,
+        n_hydroatlas=None,
+        hydroatlas_dim=None,
+        lead_time_dim=None,
+        w_hydroatlas_basin=True,
+        w_dem_context=True,
+        w_dem_local=True,
+        w_s1=True,
+        n_predict=3,
+        weather_window_size=20,
+        context_embed_output_dim=3,
+    ):
         super().__init__()
-        self.hydro_atlas_embed = nn.Conv2d(n_hydroatlas, hydroatlas_dim, kernel_size=3, padding=1)
+        self.w_hydroatlas_basin = w_hydroatlas_basin
+        self.w_dem_context = w_dem_context
+        self.w_dem_local = w_dem_local
+        self.w_s1 = w_s1
+        self.weather_window_size = weather_window_size
+
+        assert (
+            self.w_s1 or self.w_dem_local
+        ), "Must provide either s1 or dem local to produce local scale predictions"
+        assert (self.w_s1 and (lead_time_dim is not None)) or (
+            (not self.w_s1) and (lead_time_dim is None)
+        ), "If you provide s1, you must also provide lead_time_dim. If not, don't provide it."
+
+        # Create context embedding layers
+        context_embed_input_dim = n_weather
+        if self.w_hydroatlas_basin:
+            self.hydro_atlas_embed = nn.Conv2d(
+                n_hydroatlas, hydroatlas_dim, kernel_size=3, padding=1
+            )
+            context_embed_input_dim += hydroatlas_dim
+        if self.w_dem_context:
+            context_embed_input_dim += 1
         self.context_embed = utae.UTAE(
-            n_context + hydroatlas_dim + 1,
+            context_embed_input_dim,
             encoder_widths=[32, 32],
             decoder_widths=[32, 32],
-            out_conv=[3],
+            out_conv=[context_embed_output_dim],
         )
-        self.fine_embed = utae.UTAE(6, out_conv=[32, n_predict])
+
+        # Create local embedding/prediction layers
+        local_input_dim = context_embed_output_dim
+        if self.w_dem_local:
+            local_input_dim += 1
+        if self.w_s1:
+            local_input_dim += 2
+            self.lead_m = 3
+            self.lead_n = 5  # see self.get_lead_time_idx for details
+            self.len_lead = (
+                weather_window_size + self.lead_m * weather_window_size // self.lead_n + 1
+            )
+            self.lead_time_embedding = nn.Embedding(self.len_lead, lead_time_dim)
+        self.local_embed = utae.UTAE(
+            local_input_dim,
+            encoder_widths=[64, 64, 64, 128],
+            decoder_widths=[64, 64, 64, 128],
+            out_conv=[64, n_predict],
+            cond_dim=lead_time_dim,
+        )
+
+    def get_lead_time_idx(self, lead):
+        # Get the index into the embedding based on lead.
+        # For normal FiLM conditioning, lead == idx, but here we're encoding an unbounded value.
+        # So, we apply a simple remapping scheme for values outside the weather window.
+        # 1. For lead times within the weather window size, lead == idx
+        lead_copy = lead.clone()
+        # 2. For lead times outside the weather window, we chunk by self.lead_n days
+        outside_window = lead > self.weather_window_size
+        chunked_idx = (lead[outside_window] - self.weather_window_size) // self.lead_n
+        lead_copy[outside_window] = chunked_idx + self.weather_window_size
+        # 3. For leads too far away in time (lead_m * window size) we just set it to the last idx
+        # This allows the model to know how to use soil moisture in the S1 images,
+        # regardless of how old. e.g. the model can ignore it if it wants.
+        lead_copy[lead > (self.lead_m * self.weather_window_size)] = self.len_lead - 1
+        return lead_copy
 
     def forward(self, ex):
         B, N, cC, cH, cW = ex["era5_land"].shape
-        fC, fH, fW = ex["s1"].shape[-3:]
-        batch_positions = torch.arange(0, N).reshape((1, N)).repeat((B, 1)).to(ex["s1"].device)
+        batch_positions = (
+            torch.arange(0, N).reshape((1, N)).repeat((B, 1)).to(ex["era5_land"].device)
+        )
+        if self.w_s1:
+            example_local = ex["s1"]
+        else:
+            example_local = ex["dem_local"]
+        fH, fW = example_local.shape[-2:]
 
         # Process context inputs
-        embedded_hydro_atlas_raster = self.hydro_atlas_embed(ex["hydroatlas_basin"])
-        context_statics_lst = [embedded_hydro_atlas_raster, ex["dem_context"]]
+        context_statics_lst = []
+        if self.w_hydroatlas_basin:
+            embedded_hydro_atlas_raster = self.hydro_atlas_embed(ex["hydroatlas_basin"])
+            context_statics_lst.append(embedded_hydro_atlas_raster)
+        if self.w_dem_context:
+            context_statics_lst.append(ex["dem_context"])
         # Add the statics in at every step
-        context_statics = (
-            torch.cat(context_statics_lst, dim=1).unsqueeze(1).repeat((1, N, 1, 1, 1))
-        )
-        context_inp = torch.cat([ex["era5_land"], ex["era5"], context_statics], dim=2)
+        if self.w_hydroatlas_basin or self.w_dem_context:
+            context_statics = (
+                torch.cat(context_statics_lst, dim=1).unsqueeze(1).repeat((1, N, 1, 1, 1))
+            )
+            context_inp = torch.cat([ex["era5_land"], ex["era5"], context_statics], dim=2)
+        else:
+            context_inp = torch.cat([ex["era5_land"], ex["era5"]], dim=2)
+        print("context:", context_inp.shape)
         context_embedded = self.context_embed(context_inp, batch_positions)
 
         # Select the central 2x2 pixels and average
         ylo, yhi = cH // 2 - 1, cH // 2 + 3
         xlo, xhi = cW // 2 - 1, cW // 2 + 3
-        context_out = context_embedded[:, :, ylo:yhi, xlo:xhi].mean(axis=(2, 3), keepdims=True)
+        context_out = context_embedded[:, 0, :, ylo:yhi, xlo:xhi].mean(axis=(2, 3), keepdims=True)
         context_out_repeat = context_out.repeat((1, 1, fH, fW))
 
-        # Process fine inputs
-        fine_inp = torch.cat([context_out_repeat, ex["s1"], ex["dem_local"]], dim=1)
+        # Get Lead time embedding indexes
+        if self.w_s1:
+            lead_idx = self.get_lead_time_idx(ex["s1_lead_days"])
+            lead = self.lead_time_embedding(lead_idx)
+        else:
+            lead = None
+
+        # Process local inputs
+        local_lst = [context_out_repeat]
+        if self.w_s1:
+            local_lst.append(ex["s1"])
+        if self.w_dem_local:
+            local_lst.append(ex["dem_local"])
+        local_inp = torch.cat(local_lst, dim=1)
         # Pretend it's temporal data with one time step for utae
-        fine_inp = fine_inp[:, None]
-        out = self.fine_embed(fine_inp, batch_positions[:, :1])
+        local_inp = local_inp[:, None]
+        print("local:  ", local_inp.shape)
+        out = self.local_embed(local_inp, batch_positions[:, :1], lead=lead)
         return out
 
 
 if __name__ == "__main__":
+    # For debugging
     B, T = 8, 12
     cH, cW = 32, 32
     fH, fW = 224, 224
@@ -53,6 +150,7 @@ if __name__ == "__main__":
     hydroatlas_dim = 16
     n_era5 = 16
     n_era5_land = 8
+    lead_time_dim = 16
     ex = {
         "era5": torch.randn((B, T, n_era5, cH, cW)).cuda(),
         "era5_land": torch.randn((B, T, n_era5_land, cH, cW)).cuda(),
@@ -60,8 +158,48 @@ if __name__ == "__main__":
         "dem_context": torch.randn((B, 1, cH, cW)).cuda(),
         "s1": torch.randn(B, 2, fH, fW).cuda(),
         "dem_local": torch.randn((B, 1, fH, fW)).cuda(),
+        "s1_lead_days": torch.randint(0, 20, (B,)).cuda(),
     }
-    model = TwoUTAE(n_hydroatlas, hydroatlas_dim, (n_era5 + n_era5_land))
+    model = TwoUTAE((n_era5 + n_era5_land), n_hydroatlas, hydroatlas_dim, lead_time_dim)
     model = model.cuda()
+    model1 = TwoUTAE((n_era5 + n_era5_land), lead_time_dim=lead_time_dim, w_hydroatlas_basin=False)
+    model1 = model1.cuda()
+    model2 = TwoUTAE(
+        (n_era5 + n_era5_land),
+        lead_time_dim=lead_time_dim,
+        w_hydroatlas_basin=False,
+        w_dem_context=False,
+    )
+    model2 = model2.cuda()
+    model3 = TwoUTAE(
+        (n_era5 + n_era5_land),
+        lead_time_dim=lead_time_dim,
+        w_hydroatlas_basin=False,
+        w_dem_context=False,
+    )
+    model3 = model3.cuda()
+    model4 = TwoUTAE(
+        (n_era5 + n_era5_land), w_hydroatlas_basin=False, w_dem_context=False, w_s1=False
+    )
+    model4 = model4.cuda()
+    model5 = TwoUTAE(
+        (n_era5 + n_era5_land),
+        lead_time_dim=lead_time_dim,
+        w_hydroatlas_basin=False,
+        w_dem_context=False,
+        w_dem_local=False,
+    )
+    model5 = model5.cuda()
+    print("Model")
     out = model(ex)
+    print("Model 1")
+    out = model1(ex)
+    print("Model 2")
+    out = model2(ex)
+    print("Model 3")
+    out = model3(ex)
+    print("Model 4")
+    out = model4(ex)
+    print("Model 5")
+    out = model5(ex)
     print(out.shape)
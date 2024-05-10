Added normalisation to the model. Plus bugfix for exporting kurosiwo S1.

gff/dataloaders.py
@@ -95,7 +95,7 @@ class FloodForecastDataset(torch.utils.data.Dataset):
         self.C = C
         self.floodmap_path = self.folder / "floodmaps" / self.C["floodmap"]
         self.valid_date_range = valid_date_range
-        self.meta_fnames = meta_fnames
+        self.meta_fnames = sorted(meta_fnames)
         self.metas = self.load_tile_metas()
         self.tiles = self.load_tile_geoms(self.metas)
 
@@ -141,8 +141,8 @@ class FloodForecastDataset(torch.utils.data.Dataset):
 
         date_str = datetime.datetime.fromisoformat(meta["pre1_date"]).strftime("%Y-%m-%d")
         if meta["type"] == "kurosiwo":
-            fname = f"{meta['info']['actiid']}-{meta['info']['aoiid']}-{date_str}.tif"
-            s1_fname = "kurosiwo" / fname
+            fname = f"{meta['info']['actid']}-{meta['info']['aoiid']}-{date_str}.tif"
+            s1_fname = Path("kurosiwo") / fname
         elif meta["type"] == "generated":
             s1_fname = f"{meta['FLOOD']}-{meta['HYBAS_ID']}-{date_str}.tif"
 
@@ -246,15 +246,16 @@ def sometimes_things_are_lists(original_batch, as_list=["continent", "geom", "co
     return result
 
 
-def read_partitions(folder: Path, test_partition: int):
+def read_partitions(folder: Path, fold: int):
     # Split index defines the partition index to use as test.
+    test_partition = fold
     val_partition = (test_partition + 1) % gff.constants.N_PARTITIONS
     val_partition_fname = f"floodmap_partition_{val_partition}.txt"
     test_partition_fname = f"floodmap_partition_{test_partition}.txt"
     train_fnames = []
     val_fnames = []
     test_fnames = []
-    for fpath in list((folder / "partitions/").glob("floodmap_partition_?.txt")):
+    for fpath in list((folder / "partitions").glob("floodmap_partition_?.txt")):
         fnames = pandas.read_csv(fpath, header=None)[0].values.tolist()
         if fpath.name == test_partition_fname:
             test_fnames.extend(fnames)
@@ -266,13 +267,15 @@ def read_partitions(folder: Path, test_partition: int):
     return train_fnames, val_fnames, test_fnames
 
 
-def get_valid_date_range(folder, all_fnames):
+def get_valid_date_range(folder, all_fnames, weather_window: int):
     era5_date_fmt = "%Y-%m"
+    weather_delta = datetime.timedelta(days=weather_window)
     # filenames like *-YYYY-mm.tif
     era5_filenames = list((folder / gff.constants.ERA5_FOLDER).glob("*"))
     era5_filenames.sort()
     era5_start_str = "-".join(era5_filenames[0].stem.split("-")[-2:])
     era5_start = datetime.datetime.strptime(era5_start_str, era5_date_fmt)
+    era5_start -= weather_delta
     era5_end_str = "-".join(era5_filenames[-1].stem.split("-")[-2:])
     era5_end = datetime.datetime.strptime(era5_end_str, era5_date_fmt)
 
@@ -280,12 +283,13 @@ def get_valid_date_range(folder, all_fnames):
     era5L_filenames.sort()
     era5L_start_str = "-".join(era5L_filenames[0].stem.split("-")[-2:])
     era5L_start = datetime.datetime.strptime(era5L_start_str, era5_date_fmt)
+    era5L_start -= weather_delta
     era5L_end_str = "-".join(era5L_filenames[-1].stem.split("-")[-2:])
     era5L_end = datetime.datetime.strptime(era5L_end_str, era5_date_fmt)
 
     fname_dates = []
     for fname in all_fnames:
-        # Ours is is FLOOD-BASIN-YYYY-MM-DD-YYYY-MM-DD-YYYY-MM-DD-meta.json
+        # Generated is is FLOOD-BASIN-YYYY-MM-DD-YYYY-MM-DD-YYYY-MM-DD-meta.json
         # kurosiwo is ACT_AOI_YYYY-MM-DD-meta.json
         date_str = "-".join(fname.split("_")[-1].split("-")[-4:-1])
         fname_dates.append(datetime.datetime.fromisoformat(date_str))
@@ -304,7 +308,7 @@ def create(C, generator):
         train_ds = DebugFloodForecastDataset(data_folder, C)
         test_ds = DebugFloodForecastDataset(data_folder, C)
     elif C["dataset"] == "forecast_dataset":
-        train_fnames, val_fnames, test_fnames = read_partitions(data_folder, C["test_partition"])
+        train_fnames, val_fnames, test_fnames = read_partitions(data_folder, C["fold"])
         all_fnames = sum([train_fnames, val_fnames, test_fnames], start=[])
         valid_date_range = get_valid_date_range(data_folder, all_fnames)
         train_ds = FloodForecastDataset(data_folder, C, valid_date_range, meta_fnames=train_fnames)

gff/models/creation.py
@@ -1,7 +1,42 @@
+import torch
+import gff.normalisation
+
+
+def get_norms(C):
+    norms = {}
+
+    era5_norm = gff.normalisation.get_era5_norm()[C["era5_keys"]]
+    era5_mean = torch.tensor(era5_norm.mean.values).reshape((1, 1, -1, 1, 1))
+    era5_std = torch.tensor(era5_norm.std.values).reshape((1, 1, -1, 1, 1))
+    norms["era5"] = (era5_mean, era5_std)
+
+    era5l_norm = gff.normalisation.get_era5l_norm()[C["era5_land_keys"]]
+    era5l_mean = torch.tensor(era5l_norm.mean.values).reshape((1, 1, -1, 1, 1))
+    era5l_std = torch.tensor(era5l_norm.std.values).reshape((1, 1, -1, 1, 1))
+    norms["era5_land"] = (era5l_mean, era5l_std)
+
+    if "hydroatlas" in C["data_sources"]:
+        hydroatlas_norm = gff.normalisation.get_hydroatlas_norm()[C["hydroatlas_bands"]]
+        hydroatlas_mean = torch.tensor(hydroatlas_norm.mean.values).reshape((1, -1, 1, 1))
+        hydroatlas_std = torch.tensor(hydroatlas_norm.std.values).reshape((1, -1, 1, 1))
+        norms["hydroatlas"] = (hydroatlas_mean, hydroatlas_std)
+
+    if "dem_local" in C["data_sources"] or "dem_context" in C["data_sources"]:
+        dem_norm = gff.normalisation.get_dem_norm(fold=C["fold"])
+        dem_mean = torch.tensor(dem_norm.mean.values).reshape((1, -1, 1, 1))
+        dem_std = torch.tensor(dem_norm.std.values).reshape((1, -1, 1, 1))
+        norms["dem"] = (dem_mean, dem_std)
+
+    if "s1" in C["data_sources"]:
+        s1_norm = gff.normalisation.get_s1_norm(fold=C["fold"])
+        s1_mean = torch.tensor(s1_norm.mean.values).reshape((1, -1, 1, 1))
+        s1_std = torch.tensor(s1_norm.std.values).reshape((1, -1, 1, 1))
+        norms["s1"] = (s1_mean, s1_std)
+
+    return norms
+
+
 def create(C):
-    n_hydroatlas = len(C["hydroatlas_keys"])
-    n_era5 = len(C["era5_keys"])
-    n_era5_land = len(C["era5_land_keys"])
     if C["model"] == "debug_model":
         from . import debug_model
 
@@ -9,11 +44,15 @@ def create(C):
     elif C["model"] == "two_utae":
         from . import two_utae
 
+        norms = get_norms(C)
+
         return two_utae.TwoUTAE(
-            (n_era5 + n_era5_land),
-            n_hydroatlas,
+            C["hydroatlas_keys"],
+            C["era5_keys"],
+            C["era5_land_keys"],
             C["hydroatlas_dim"],
             C["lead_time_dim"],
+            norms=norms,
             w_hydroatlas_basin=("hydroatlas_basin" in C["data_sources"]),
             w_dem_context=("dem_context" in C["data_sources"]),
             w_dem_local=("dem_local" in C["data_sources"]),

gff/models/two_utae.py
@@ -1,21 +1,41 @@
 import torch
 import torch.nn as nn
 
-from . import utae
+# from . import utae
+import gff.models.utae as utae
 
 
-def nans_to_zero(t: torch.Tensor):
-    t[torch.isnan(t)] = 0
-    return t
+def nans_to_zero(t: torch.Tensor | None):
+    if t is not None:
+        t[torch.isnan(t)] = 0
+        return t
+
+
+def get_empty_norms(era5_bands: int, era5l_bands: int, hydroatlas_bands: int):
+    return {
+        "era5": (torch.zeros((1, 1, era5_bands, 1, 1)), torch.ones((1, 1, era5_bands, 1, 1))),
+        "era5_land": (
+            torch.zeros((1, 1, era5l_bands, 1, 1)),
+            torch.ones((1, 1, era5l_bands, 1, 1)),
+        ),
+        "hydroatlas_basin": (
+            torch.zeros((1, hydroatlas_bands, 1, 1)),
+            torch.ones((1, hydroatlas_bands, 1, 1)),
+        ),
+        "dem": (torch.zeros((1, 1, 1, 1)), torch.ones((1, 1, 1, 1))),
+        "s1": (torch.zeros((1, 2, 1, 1)), torch.ones((1, 2, 1, 1))),
+    }
 
 
 class TwoUTAE(nn.Module):
     def __init__(
         self,
-        n_weather,
-        n_hydroatlas=None,
+        era5_bands,
+        era5l_bands,
+        hydroatlas_bands=[],
         hydroatlas_dim=None,
         lead_time_dim=None,
+        norms={},
         w_hydroatlas_basin=True,
         w_dem_context=True,
         w_dem_local=True,
@@ -25,24 +45,40 @@ class TwoUTAE(nn.Module):
         context_embed_output_dim=3,
     ):
         super().__init__()
+        self.era5_bands = era5_bands
+        self.era5l_bands = era5l_bands
+        self.hydroatlas_bands = hydroatlas_bands
         self.w_hydroatlas_basin = w_hydroatlas_basin
         self.w_dem_context = w_dem_context
         self.w_dem_local = w_dem_local
         self.w_s1 = w_s1
         self.weather_window_size = weather_window_size
 
+        # Store normalisation info on model
+        # (To load model weights, the shapes must be identical; so use empty if not known at init)
+        empty_norms = get_empty_norms(len(era5_bands), len(era5l_bands), len(hydroatlas_bands))
+        for key in ["era5", "era5_land", "hydroatlas_basin", "dem", "s1"]:
+            if key in norms:
+                mean, std = norms[key]
+            else:
+                mean, std = empty_norms[key]
+            self.register_buffer(f"{key}_mean", mean)
+            self.register_buffer(f"{key}_std", std)
+
         assert (
             self.w_s1 or self.w_dem_local
         ), "Must provide either s1 or dem local to produce local scale predictions"
         assert (self.w_s1 and (lead_time_dim is not None)) or (
             (not self.w_s1) and (lead_time_dim is None)
-        ), "If you provide s1, you must also provide lead_time_dim. If not, don't provide it."
+        ), "If you provide s1, you must also provide lead_time_dim. If not, you shouldn't."
 
         # Create context embedding layers
-        context_embed_input_dim = n_weather
+        self.n_weather = len(era5_bands) + len(era5l_bands)
+        self.n_hydroatlas = len(hydroatlas_bands)
+        context_embed_input_dim = self.n_weather
         if self.w_hydroatlas_basin:
             self.hydro_atlas_embed = nn.Conv2d(
-                n_hydroatlas, hydroatlas_dim, kernel_size=3, padding=1
+                self.n_hydroatlas, hydroatlas_dim, kernel_size=3, padding=1
             )
             context_embed_input_dim += hydroatlas_dim
         if self.w_dem_context:
@@ -74,6 +110,18 @@ class TwoUTAE(nn.Module):
             cond_dim=lead_time_dim,
         )
 
+    def normalise(self, ex, key, suffix=None):
+        if suffix is not None:
+            ex_key = f"{key}_{suffix}"
+        else:
+            ex_key = key
+        if ex_key in ex:
+            data = ex[ex_key]
+            km = f"{key}_mean"
+            ks = f"{key}_std"
+            data = (data - getattr(self, km)) / getattr(self, ks)
+            return data
+
     def get_lead_time_idx(self, lead):
         # Get the index into the embedding based on lead.
         # For normal FiLM conditioning, lead == idx, but here we're encoding an unbounded value.
@@ -101,27 +149,35 @@ class TwoUTAE(nn.Module):
             example_local = ex["dem_local"]
         fH, fW = example_local.shape[-2:]
 
+        # Normalise inputs
+        era5_inp = self.normalise(ex, "era5")
+        era5l_inp = self.normalise(ex, "era5_land")
+        hydroatlas_inp = self.normalise(ex, "hydroatlas_basin")
+        dem_context_inp = self.normalise(ex, "dem", "context")
+        dem_local_inp = self.normalise(ex, "dem", "local")
+        s1_inp = self.normalise(ex, "s1")
+
         # These inputs might have nan
-        # TODO: Apply standardisation on all inputs before running
-        nans_to_zero(ex["dem_context"])
-        nans_to_zero(ex["dem_local"])
-        nans_to_zero(ex["era5_land"])
+        era5_inp = nans_to_zero(era5_inp)
+        hydroatlas_inp = nans_to_zero(hydroatlas_inp)
+        dem_context_inp = nans_to_zero(dem_context_inp)
+        dem_local_inp = nans_to_zero(dem_local_inp)
 
         # Process context inputs
         context_statics_lst = []
         if self.w_hydroatlas_basin:
-            embedded_hydro_atlas_raster = self.hydro_atlas_embed(ex["hydroatlas_basin"])
+            embedded_hydro_atlas_raster = self.hydro_atlas_embed(hydroatlas_inp)
             context_statics_lst.append(embedded_hydro_atlas_raster)
         if self.w_dem_context:
-            context_statics_lst.append(ex["dem_context"])
+            context_statics_lst.append(dem_context_inp)
         # Add the statics in at every step
         if self.w_hydroatlas_basin or self.w_dem_context:
             context_statics = (
                 torch.cat(context_statics_lst, dim=1).unsqueeze(1).repeat((1, N, 1, 1, 1))
             )
-            context_inp = torch.cat([ex["era5_land"], ex["era5"], context_statics], dim=2)
+            context_inp = torch.cat([era5l_inp, era5_inp, context_statics], dim=2)
         else:
-            context_inp = torch.cat([ex["era5_land"], ex["era5"]], dim=2)
+            context_inp = torch.cat([era5l_inp, era5_inp], dim=2)
         context_embedded = self.context_embed(context_inp, batch_positions)
 
         # Select the central 2x2 pixels and average
@@ -140,9 +196,9 @@ class TwoUTAE(nn.Module):
         # Process local inputs
         local_lst = [context_out_repeat]
         if self.w_s1:
-            local_lst.append(ex["s1"])
+            local_lst.append(s1_inp)
         if self.w_dem_local:
-            local_lst.append(ex["dem_local"])
+            local_lst.append(dem_local_inp)
         local_inp = torch.cat(local_lst, dim=1)
         # Pretend it's temporal data with one time step for utae
         local_inp = local_inp[:, None]
@@ -170,30 +226,38 @@ if __name__ == "__main__":
         "dem_local": torch.randn((B, 1, fH, fW)).cuda(),
         "s1_lead_days": torch.randint(0, 20, (B,)).cuda(),
     }
-    model = TwoUTAE((n_era5 + n_era5_land), n_hydroatlas, hydroatlas_dim, lead_time_dim)
+    era5_bands = list(range(n_era5))
+    era5l_bands = list(range(n_era5_land))
+    hydroatlas_bands = list(range(n_hydroatlas))
+    model = TwoUTAE(era5_bands, era5l_bands, hydroatlas_bands, hydroatlas_dim, lead_time_dim)
     model = model.cuda()
-    model1 = TwoUTAE((n_era5 + n_era5_land), lead_time_dim=lead_time_dim, w_hydroatlas_basin=False)
+    model1 = TwoUTAE(
+        era5_bands, era5l_bands, lead_time_dim=lead_time_dim, w_hydroatlas_basin=False
+    )
     model1 = model1.cuda()
     model2 = TwoUTAE(
-        (n_era5 + n_era5_land),
+        era5_bands,
+        era5l_bands,
         lead_time_dim=lead_time_dim,
         w_hydroatlas_basin=False,
         w_dem_context=False,
     )
     model2 = model2.cuda()
     model3 = TwoUTAE(
-        (n_era5 + n_era5_land),
+        era5_bands,
+        era5l_bands,
         lead_time_dim=lead_time_dim,
         w_hydroatlas_basin=False,
         w_dem_context=False,
     )
     model3 = model3.cuda()
     model4 = TwoUTAE(
-        (n_era5 + n_era5_land), w_hydroatlas_basin=False, w_dem_context=False, w_s1=False
+        era5_bands, era5l_bands, w_hydroatlas_basin=False, w_dem_context=False, w_s1=False
     )
     model4 = model4.cuda()
     model5 = TwoUTAE(
-        (n_era5 + n_era5_land),
+        era5_bands,
+        era5l_bands,
         lead_time_dim=lead_time_dim,
         w_hydroatlas_basin=False,
         w_dem_context=False,
@@ -202,6 +266,8 @@ if __name__ == "__main__":
     model5 = model5.cuda()
     print("Model")
     out = model(ex)
+    # Only hydroatlas will cause problems if provided after saying we wouldn't
+    ex.pop("hydroatlas_basin")
     print("Model 1")
     out = model1(ex)
     print("Model 2")